a86bceb0bb99ba61686060f6212efeba4bdc12d6
PayalPatel03 <p6579113@gmail.com>
2025-01-28 17:51:33 +0530

    done

---
+++ day1/script.js
@@ -0,0 +1,25 @@
+// for printing message in console
+// console.log("helllo world!!");
+
+// printing message in frontpage
+// document.write("Javascript Programer");
+
+// for alert message
+// alert("Invalid");  
+
+let title=document.getElementById("title");
+let text=document.getElementById("text");
+
+
+title.innerHTML="<big>Hello</big>Coders";
+text.innerText="payal patel komal patel akshar pharekh milan mandaviya";
+
+// //prompt message 
+// let name=prompt("What is your name:-");
+// console.log("Hello, "+name);
+
+let p = document.createElement("p");
+p.textContent = "This is for a paragraph.";
+document.body.appendChild(p);
+
+
