2c2386222a243c47cb6274b166fa8ad9d6e2833a
PayalPatel03 <p6579113@gmail.com>
2025-01-29 17:29:57 +0530

    done

---
+++ variable/js/script.js
@@ -0,0 +1,30 @@
+//variables
+let age = 20;
+const pi = 3.14;
+let name = 'Payal';
+let valid = false;
+let value = null;
+
+console.log("typeof age:" + typeof age);
+console.log("typeof pi:" + typeof pi);
+console.log("typeof name:" + typeof name);
+console.log("typeof valid:" + typeof valid);
+console.log("typeof value:" + typeof value);
+
+//arithmetic operator
+let a=10;
+let b=5;
+
+console.log(a+b);
+console.log(a-b);
+console.log("Multiplication: "+ a*b);
+console.log("Division: "+ a/b);
+console.log("Exponetional:"+ a**2);
+
+
+
+
+
+
+
+
