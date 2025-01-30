59721b64aaafbdbc2561d3c376d16f375dbd22f1
PayalPatel03 <p6579113@gmail.com>
2025-01-30 17:15:23 +0530

    donee

--- daily_pract/operator/js/script.js
+++ daily_pract/operator/js/script.js
@@ -1,6 +1,13 @@
 //arithmetic operator
 let a=10;
 let b=5;
+//swaping without third variable
+let c=20;
+let d=30;
+//swaping using third varaible
+let x=12;
+let y=11;
+
 
 console.log(a+b);
 console.log(a-b);
@@ -11,3 +18,26 @@ console.log("5 cube is:"+ b**3);
 console.log("10 square is:"+ a**6);
 console.log("Post increment:"+ a++);
 console.log("Pre increment:"+ ++a);
+
+//swaping without third variable
+console.log("Before swaping c is:"+ c);
+console.log("Before swaping d is:"+ d);
+c=c+d;
+d=c-d;
+c=c-d;
+
+console.log("After swaping c is:"+ c);
+console.log("After swaping d is:"+ d);
+
+//swaping with third varaible
+console.log("Before swaping x is:"+ x);
+console.log("Before swaping y is:"+ y);
+let temp=x;
+x=y;
+y=temp;
+
+console.log("After swaping x is:"+ x);
+console.log("After swaping y is:"+ y);
+
+
+
