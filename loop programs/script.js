856cc9b605bbc51667122a3b9d02012c0a47eb78
PayalPatel03 <p6579113@gmail.com>
2025-02-06 21:48:47 +0530

    done

---
+++ loop programs/script.js
@@ -0,0 +1,48 @@
+//prog-1 Print numbers 1 to 10 using for loop
+// let str='';
+// for(let i=1;i<=10;i++){
+//     str += i+' , ';
+// }
+// console.log(str);
+
+//prog-2 Print numbers 1 to 10 using while loop
+// let str='';
+// let i=1;
+// while(i<=10){
+//     str += i+' , ';
+//     i++;
+// }
+// console.log(str);
+
+//prog-3 Print numbers 1 to 10 using do-while loop
+// let i=1;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<=10);
+
+//prog-4 Sum of first 10 natural numbers (for loop)
+// let ans=0;
+// for(i=1;i<=10;i++){
+//     ans+=i;
+// }
+// console.log(`Answer:${ans}`);
+
+
+//prog-5  Sum of first 10 natural numbers (while loop)
+// let ans=0;
+// let i=1;
+// while(i<=10){
+//     ans+=i;
+//     i++;
+// }
+// console.log(`Answer:${ans}`);
+
+//prog-6 Sum of first 10 natural numbers (do-while loop)
+let ans = 0;
+let i = 1;
+do {
+ ans += i;
+ i++;
+} while (i <= 10);
+console.log(`Answer:${ans}`);
