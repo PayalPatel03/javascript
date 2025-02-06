4d8c671f53221446f528cf9d2666aedee497aeb7
PayalPatel03 <p6579113@gmail.com>
2025-02-06 17:41:10 +0530

    donee

---
+++ daily_pract/looping/script.js
@@ -0,0 +1,30 @@
+//entry control=>for ,while
+//exit control=>do while
+//diffrence bt while n do-while
+
+
+// for loop
+// for(let i=1;i<=5;i++){
+//     console.log(i);
+// }
+
+//in a line
+// let str='';
+// for(let i=1;i<=5;i++){
+//     str += i+' , '; 
+// }
+// console.log(str);
+
+//while loop
+// let i=1;
+// while(i<=5){
+//     console.log(i);
+//     i++;
+// }
+
+//do-while loop
+let i=1;
+do{
+    console.log(i);
+    i++;
+}while(i<=5);
