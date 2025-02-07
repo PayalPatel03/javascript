6094edc6d93d18a6edcfd3b775c2548dcb6a8e53
PayalPatel03 <p6579113@gmail.com>
2025-02-07 18:10:43 +0530

    dne

--- daily_pract/patterns/script.js
+++ daily_pract/patterns/script.js
@@ -45,3 +45,69 @@
 //     str+="<br>";
 // }
 // document.write(str);
+
+
+//1 2 3 4 5
+//1 2 3 4
+//1 2 3
+//1 2
+//1
+
+// let str='';
+// for(let i=5;i>=1;i--){
+//     for(let j=1;j<=i;j++){
+//         str+=j+" ";
+//     }
+//     str+="<br>";
+// }
+// document.write(str);
+
+//5 4 3 2 1
+//4 3 2 1
+//3 2 1
+//2 1
+//1
+
+// let str='';
+// for(let i=5;i>=1;i--){
+//     for(j=i;j>=1;j--){
+//         str+=j+" ";
+//     }
+//     str+="<br>";
+// }
+// document.write(str);
+
+//1
+//2 1
+//3 2 1
+//4 3 2 1
+//5 4 3 2 1
+
+// let str='';
+// for(i=1;i<=5;i++){
+//     for(j=i;j>=1;j--){
+//         str+=j+" ";
+//     }
+//     str+="<br>";
+// }
+// document.write(str);
+
+//fibbonacci series 0 1 1 2 3 5 8 13 21
+// let a=0 ,b=1;
+// let count=0;
+// let n=7; //here n is the number of terms
+// do{
+//     document.write(a + " ");
+//     let sum=a+b;
+//     a=b;
+//     b=sum;
+//     count++;
+// }while(count<n);
+
+
+//1
+//1 2
+//1 3 5
+//1 6 11 16
+//1 17 33 49 65
+
