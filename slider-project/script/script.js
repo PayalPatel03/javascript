9a8ff941e22b0e4fe5373571a2f59dc58e11fbf1
PayalPatel03 <p6579113@gmail.com>
2025-03-01 18:07:16 +0530

    update

--- slider-project/script/script.js
+++ slider-project/script/script.js
@@ -1,14 +1,16 @@
 let slideIndex = 1;
+let slideItem = document.querySelectorAll(".slider-item");
+let currSlideInd = document.getElementById("current-slide");
+let totalSlideInd = document.getElementById("total-slide");
+let dots=document.querySelectorAll(".dot");
+
+totalSlideInd.textContent = slideItem.length;
 
 let currSlide = (n) => {
     displaySlide((slideIndex += n));
 };
 
 let displaySlide = (n) => {
-    let slideItem = document.querySelectorAll(".slider-item");
-    console.log(n);
-    console.log(slideIndex);
-
     if (n > slideItem.length) {
         slideIndex = 1;
     }
@@ -16,10 +18,18 @@ let displaySlide = (n) => {
         slideIndex = slideItem.length;
     }
 
-    for (let i = 0; i < slideItem.length; i++) {
-        slideItem[i].classList.remove("active");
-    }
+    slideItem.forEach((slide) => slide.classList.remove("active"));
+    dots.forEach((dot)=>dot.classList.remove("active"));
+
     slideItem[slideIndex - 1].classList.add("active");
+    dots[slideIndex-1].classList.add("active");
+
+    currSlideInd.textContent = slideIndex;
 };
 
+
 displaySlide(slideIndex);
+
+// setInterval(() => {
+//     currSlide(1);
+// }, 3000);
