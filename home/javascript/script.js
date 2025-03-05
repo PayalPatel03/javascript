f58fd22c2958a432cbe3b39926846d75ef806102
PayalPatel03 <p6579113@gmail.com>
2025-03-05 22:04:09 +0530

    done

---
+++ home/javascript/script.js
@@ -0,0 +1,9 @@
+// //js
+// document.getElementById("toggleButton").addEventListener("click", function() {
+//     var featureBox = document.getElementById("featureBox");
+//     if (featureBox.style.display === "none" || featureBox.style.display === "") {
+//         featureBox.style.display = "block";
+//     } else {
+//         featureBox.style.display = "none";
+//     }
+// });
