95a58584d5a9883239f535ceba07bc2e59bf9b91
PayalPatel03 <p6579113@gmail.com>
2025-03-07 17:24:27 +0530

    done

--- calculator/script.js
+++ calculator/script.js
@@ -5,11 +5,30 @@ let display=document.querySelector('#display');
 buttons.forEach((button)=>{
     button.addEventListener('click',(e)=>{
         let value=e.target.innerText;
+        let displayValue=display.innerText;
+        let last=displayValue[displayValue.length-1];
+        let removeLastStr=displayValue.slice(0,-1);
+        // console.log(removeLastStr);
+        
+
         if(value=='='){
             display.value=eval(display.value);
         }
         else if(value=='C'){
             display.value='';
+        }
+        else if(array.includes(value)){
+            if(last== value){
+                display.value=display.value;
+            }
+            else{
+                if(array.includes(last)){
+                    display.value=removeLastStr+value;
+                }
+                else{
+                    display.value+=value;
+                }
+            }
         }
         else{
             display.value+=value;
