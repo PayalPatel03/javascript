99fc920fcb164069cfed85b0c2cbf9209d0e4a96
PayalPatel03 <p6579113@gmail.com>
2025-03-13 17:09:32 +0530

    done

---
+++ daily_pract/OOPs/constructor/demo.js
@@ -0,0 +1,11 @@
+//deafult const
+// class Demo{
+//     constructor(){
+//         console.log("This is from constructor.");
+//     }
+//     print(){
+//         console.log("This is from Demo.");
+//     }
+// }
+// let obj=new Demo();
+// obj.print();
