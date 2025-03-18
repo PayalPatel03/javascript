194663b9b448536523312f883bf04d608f828091
PayalPatel03 <p6579113@gmail.com>
2025-03-18 17:31:31 +0530

    done

--- daily_pract/OOPs/abstraction/demo.js
+++ daily_pract/OOPs/abstraction/demo.js
@@ -39,6 +39,22 @@ class Employee {
     }
 }
 
-// Creating an object with proper constructor parameters
-let a = new Employee("Ramesh", 33, 23456);
-a.getEmp();
+class Salary extends Employee {
+    constructor(name, age, salary, hra, ma, pf) {
+        super(name, age, salary);
+        this.hra = hra;
+        this.ma = ma;
+        this.pf = pf;
+        this.grossSalary = this.hra + this.ma - this.pf;
+        this.netSalary = this.grossSalary + this.salary;
+    }
+
+    getSal() {
+        super.getEmp();
+        console.log(`Employee grossSalary is ${this.grossSalary} and netSalary is ${this.netSalary}`);
+    }
+}
+
+
+let sal = new Salary("Bheem", 19, 60000, 3500, 2000, 4500);
+sal.getSal();
