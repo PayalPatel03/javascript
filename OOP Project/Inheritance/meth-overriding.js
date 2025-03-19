45af517b0f669b9879669c8a1bffb1457945d01d
PayalPatel03 <p6579113@gmail.com>
2025-03-19 17:09:45 +0530

    donee

---
+++ OOP Project/Inheritance/meth-overriding.js
@@ -0,0 +1,15 @@
+class Parent {
+    greet() {
+      console.log("Hello from Parent!");
+    }
+  }
+  
+  class Child extends Parent {
+    greet() {
+      console.log("Hello from Child!");
+    }
+  }
+  
+  const obj = new Child();
+  obj.greet();
+  
