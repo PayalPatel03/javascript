bd76ef579aca608a96f44316d0dd65703e531faf
PayalPatel03 <p6579113@gmail.com>
2025-03-20 17:34:23 +0530

    donee

--- scroll-header-without/script.js
+++ scroll-header-without/script.js
@@ -1,16 +1,12 @@
 let lastScrollY = window.scrollY;
 const header = document.getElementById("header");
-let hideTimeout;
 
 window.addEventListener("scroll", () => {
-    clearTimeout(hideTimeout); 
-
     if (window.scrollY > lastScrollY) {
+        // Scrolling down
         header.style.top = "-70px";
-        hideTimeout = setTimeout(() => {
-            header.style.top = "0";
-        }, 1000);
     } else {
+        // Scrolling up
         header.style.top = "0";
     }
     lastScrollY = window.scrollY;
